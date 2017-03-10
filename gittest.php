7824284a417b7e72a1b7f9f7f572dc32cb76f8b6
MaddyZ <maddyking0511@gmail.com>
2017-03-10 19:11:43 +0300

    third commit

--- gittest.php
+++ gittest.php
@@ -6,5 +6,6 @@
 </head>
 <body>
 	<h1>Hello, world!</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, magni, tempore. Iure doloremque nemo saepe quod sapiente, tempore nihil incidunt modi ipsam amet animi recusandae quia, facere quo veniam, nobis.</p>
 </body>
 </html>
